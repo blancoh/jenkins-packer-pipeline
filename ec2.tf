62b7813efce8ead987b12ad0d0202179290b23cd
Hector Blanco <blancoh@gmail.com>
2019-07-13 18:59:25 -0400

    Updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -35,22 +35,22 @@ resource "aws_security_group" "sec_web" {
   }
 }
 
-resource "aws_security_group" "sec_lb" {
-  name = "sec_elb"
-  vpc_id      = "${aws_vpc.default.id}"
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#resource "aws_security_group" "sec_lb" {
+#  name = "sec_elb"
+#  vpc_id      = "${aws_vpc.default.id}"
+#  
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  
+#  ingress {
+#    from_port = 80
+#    to_port = 80
+#    protocol = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
 
