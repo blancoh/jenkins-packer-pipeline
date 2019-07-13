6c79c3ef59fa40b3d3a29af539a281d7723b11fd
Hector Blanco <blancoh@gmail.com>
2019-07-13 19:12:52 -0400

    Updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,34 +4,3 @@ resource "aws_key_pair" "auth" {
   public_key = "${file(var.public_key_path)}"
 }
 
-#resource "aws_security_group" "sec_web" {
-#  name        = "sec_web"
-#  description = "Used for autoscale group"
-#  vpc_id      = "${aws_vpc.default.id}"
-#
-#  # HTTP access from anywhere
-#  ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  
-#  # outbound internet access
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
-#
