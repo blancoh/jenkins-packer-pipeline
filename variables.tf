c63db2d6741a2a894af09427ef29e674b37b17ef
Hector Blanco <blancoh@gmail.com>
2019-07-13 22:21:08 -0400

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -12,13 +12,8 @@ variable "aws_region" {
 # Ubuntu Bionic 18.04 LTS (x64)
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-01e6a0b85de033c99"
     us-east-1 = "ami-026c8acd92718196b"
   }
-}
-variable "dnszonename" {
-  default = "aws.example.net"
-  description = "my internal dns name"
 }
 variable "vpc_cidr" {
     default = "10.0.0.0/20"
@@ -27,24 +22,4 @@ variable "vpc_cidr" {
 variable "public_subnet_a" {
   default = "10.0.0.0/24"
   description = "Public subnet AZ A"
-}
-variable "public_subnet_b" {
-  default = "10.0.4.0/24"
-  description = "Public subnet AZ B"
-}
-variable "public_subnet_c" {
-  default = "10.0.8.0/24"
-  description = "Public subnet AZ C"
-}
-variable "private_subnet_a" {
-  default = "10.0.1.0/24"
-  description = "Private subnet AZ A"
-}
-variable "private_subnet_b" {
-  default = "10.0.5.0/24"
-  description = "Private subnet AZ B"
-}
-variable "private_subnet_c" {
-  default = "10.0.9.0/24"
-  description = "Private subnet AZ C"
 }
