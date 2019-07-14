a0634953a994a7efde01aa4cc3a87b486b1b2b28
Hector Blanco <blancoh@gmail.com>
2019-07-14 05:59:02 -0400

    Consolidated files

--- main.tf
+++
@@ -1,7 +0,0 @@
-# Specify the provider and access details
-provider "aws" {
-  region = "${var.aws_region}"
-}
-
-# Declare the data source
-data "aws_availability_zones" "available" {}
--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,16 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Declare the data source
+data "aws_availability_zones" "available" {}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
 # Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
