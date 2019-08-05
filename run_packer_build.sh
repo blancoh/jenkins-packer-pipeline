da54e0772b70240d5355dd6767343fd79a5b07c7
abelanc0 <abelanc0@gmail.com>
2019-08-05 17:38:10 -0400

    Updated run_packer_build.sh

--- run_packer_build.sh
+++ run_packer_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo $AWS_ACCESS_KEY
-/usr/local/bin/terraform output vpc_ids
-/usr/local/bin/terraform output vpc_id
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+#/usr/local/bin/terraform output vpc_ids
+#/usr/local/bin/terraform output vpc_id
 
 vpc_id=`/usr/local/bin/aws ec2 describe-vpcs --filters "Name=tag:Name,Values=Packer_VPC" --query 'Vpcs[*].VpcId' --output text`
 subnet_id=`/usr/local/bin/aws ec2 describe-subnets --filters "Name=tag:Name,Values=Packer_Subnet_A" --query 'Subnets[*].SubnetId' --output text`
