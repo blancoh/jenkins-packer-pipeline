afe098d97c089a25223a03a84ae7d8c4011e0d15
blancoh <blancoh@users.noreply.github.com>
2019-08-04 20:24:17 -0400

    Update run_packer_build.sh

--- run_packer_build.sh
+++ run_packer_build.sh
@@ -3,4 +3,3 @@
 vpc_id=`aws ec2 describe-vpcs --filters "Name=tag:Name,Values=Packer_VPC" --query 'Vpcs[*].VpcId' --output text`
 subnet_id=`aws ec2 describe-subnets --filters "Name=tag:Name,Values=Packer_Subnet_A" --query 'Subnets[*].SubnetId' --output text`
 /usr/local/bin/packer build -var "aws_vpc=$vpc_id" -var "aws_subnet=$subnet_id" packer_build.json
-
