cff61eb76a682e7613ccccb809322df6ce64c6a5
abelanc0 <abelanc0@gmail.com>
2019-08-05 17:56:26 -0400

    Updated run_packer_build.sh

--- run_packer_build.sh
+++ run_packer_build.sh
@@ -3,6 +3,6 @@
 aws_bin=`which aws`
 packer_bin=`which packer`
 
-vpc_id=`$aws_bin ec2 describe-vpcs --filters "Name=tag:Name,Values=Packer_VPC" --query 'Vpcs[*].VpcId' --output text`
-subnet_id=`$aws_bin ec2 describe-subnets --filters "Name=tag:Name,Values=Packer_Subnet_A" --query 'Subnets[*].SubnetId' --output text`
+vpc_id=$($aws_bin ec2 describe-vpcs --filters "Name=tag:Name,Values=Packer_VPC" --query 'Vpcs[*].VpcId' --output text)
+subnet_id=$($aws_bin ec2 describe-subnets --filters "Name=tag:Name,Values=Packer_Subnet_A" --query 'Subnets[*].SubnetId' --output text)
 $packer_bin build -var "aws_vpc=$vpc_id" -var "aws_subnet=$subnet_id" packer_build.json
