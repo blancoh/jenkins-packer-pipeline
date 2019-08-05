7c4fdef1483498d749132f6727e99f99e65dc4ae
abelanc0 <abelanc0@gmail.com>
2019-08-05 17:45:38 -0400

    Updated run_packer_build.sh

--- run_packer_build.sh
+++ run_packer_build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-#/usr/local/bin/terraform output vpc_ids
-#/usr/local/bin/terraform output vpc_id
+aws_bin=`which aws`
+packer_bin=`which packer`
 
-vpc_id=`/usr/local/bin/aws ec2 describe-vpcs --filters "Name=tag:Name,Values=Packer_VPC" --query 'Vpcs[*].VpcId' --output text`
-subnet_id=`/usr/local/bin/aws ec2 describe-subnets --filters "Name=tag:Name,Values=Packer_Subnet_A" --query 'Subnets[*].SubnetId' --output text`
-/usr/local/bin/packer build -var "aws_vpc=$vpc_id" -var "aws_subnet=$subnet_id" packer_build.json
+vpc_id=`$aws_bin ec2 describe-vpcs --filters "Name=tag:Name,Values=Packer_VPC" --query 'Vpcs[*].VpcId' --output text`
+subnet_id=`$aws_bin ec2 describe-subnets --filters "Name=tag:Name,Values=Packer_Subnet_A" --query 'Subnets[*].SubnetId' --output text`
+$packer_bin build -var "aws_vpc=$vpc_id" -var "aws_subnet=$subnet_id" packer_build.json
