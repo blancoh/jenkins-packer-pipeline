d5136ec977fe3f95a46370767e233cc808343af9
blancoh <blancoh@users.noreply.github.com>
2021-04-11 16:07:46 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 data "aws_availability_zones" "available" {}
 
 resource "aws_key_pair" "auth" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key_path)}"
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
 }
 
 resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
+    cidr_block = var.vpc_cidr
     enable_dns_support = true
     enable_dns_hostnames = true
     tags =  {
@@ -19,34 +19,34 @@ resource "aws_vpc" "default" {
 }
 
 resource "aws_subnet" "PublicSubnetA" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = "${var.public_subnet_a}"
+  vpc_id = aws_vpc.default.id
+  cidr_block = var.public_subnet_a
   map_public_ip_on_launch = true
   tags = {
         Name = "Packer_Subnet_A"
   }
- availability_zone = "${data.aws_availability_zones.available.names[0]}"
+ availability_zone = data.aws_availability_zones.available.names[0]
 }
 
 resource "aws_route_table_association" "PublicSubnetA" {
-    subnet_id = "${aws_subnet.PublicSubnetA.id}"
-    route_table_id = "${aws_route_table.public_route_a.id}"
+    subnet_id = aws_subnet.PublicSubnetA.id
+    route_table_id = aws_route_table.public_route_a.id
 }
 
 resource "aws_internet_gateway" "gw" {
-   vpc_id = "${aws_vpc.default.id}"
+   vpc_id = aws_vpc.default.id
     tags = {
         Name = "Packer_Internet_Gateway"
     }
 }
 
 resource "aws_route_table" "public_route_a" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = aws_vpc.default.id
   tags = {
       Name = "Packer_Route_A"
   }
   route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.gw.id}"
+        gateway_id = aws_internet_gateway.gw.id
     }
 }
