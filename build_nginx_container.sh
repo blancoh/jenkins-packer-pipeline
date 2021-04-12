6ed9b51261c5411b2ff2f17fc783616805bfb2a7
blancoh <blancoh@users.noreply.github.com>
2021-04-11 22:06:09 -0400

    Update build_nginx_container.sh

--- build_nginx_container.sh
+++ build_nginx_container.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-repo_name="nginx-dhmf"
-repo_url="522939339121.dkr.ecr.us-east-1.amazonaws.com"
+sudo repo_name="nginx-dhmf"
+sudo repo_url="522939339121.dkr.ecr.us-east-1.amazonaws.com"
 #Authenticate
-PASSWD=`aws ecr get-login-password --region us-east-1`
-docker login --username AWS -p ${PASSWD} ${repo_url}
+sudo PASSWD=`aws ecr get-login-password --region us-east-1`
+sudo docker login --username AWS -p ${PASSWD} ${repo_url}
 #Delete ECR Repository
-aws ecr delete-repository --repository-name ecrrepo001/${repo_name} --force
+sudo aws ecr delete-repository --repository-name ecrrepo001/${repo_name} --force
 #Create ECR Repository
-aws ecr create-repository --repository-name ecrrepo001/${repo_name}
+sudo aws ecr create-repository --repository-name ecrrepo001/${repo_name}
 #Clear local docker repository of old images
-for i in $(docker images | grep ${repo_name} | awk '{print $3}');do docker rmi -f $i;done
+sudo for i in $(docker images | grep ${repo_name} | awk '{print $3}');do docker rmi -f $i;done
 #Build new Nginx container
-docker build  . -t ${repo_name}
+sudo docker build  . -t ${repo_name}
 #Get Image ID
-image_id=`docker images | grep ${repo_name} | awk '{print $3}'  | sort -u`
-echo $image_id
+sudo image_id=`docker images | grep ${repo_name} | awk '{print $3}'  | sort -u`
+sudo echo $image_id
 # Tag and Push to Amazon ECR
-docker tag ${image_id} 522939339121.dkr.ecr.us-east-1.amazonaws.com/ecrrepo001/${repo_name}:latest
-docker push 522939339121.dkr.ecr.us-east-1.amazonaws.com/ecrrepo001/${repo_name}:latest
+sudo docker tag ${image_id} 522939339121.dkr.ecr.us-east-1.amazonaws.com/ecrrepo001/${repo_name}:latest
+sudo docker push 522939339121.dkr.ecr.us-east-1.amazonaws.com/ecrrepo001/${repo_name}:latest
