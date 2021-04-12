38273e4aa3899a758c596cb939fdf3739e10cdb3
blancoh <blancoh@users.noreply.github.com>
2021-04-11 22:47:36 -0400

    Update build_nginx_container.sh

--- build_nginx_container.sh
+++ build_nginx_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo repo_name="nginx-dhmf"
-sudo repo_url="522939339121.dkr.ecr.us-east-1.amazonaws.com"
+repo_name="nginx-dhmf"
+repo_url="522939339121.dkr.ecr.us-east-1.amazonaws.com"
 #Authenticate
 sudo PASSWD=`aws ecr get-login-password --region us-east-1`
 sudo docker login --username AWS -p ${PASSWD} ${repo_url}
@@ -9,11 +9,11 @@ sudo aws ecr delete-repository --repository-name ecrrepo001/${repo_name} --force
 #Create ECR Repository
 sudo aws ecr create-repository --repository-name ecrrepo001/${repo_name}
 #Clear local docker repository of old images
-sudo for i in $(docker images | grep ${repo_name} | awk '{print $3}');do docker rmi -f $i;done
+for i in $(sudo docker images | grep ${repo_name} | awk '{print $3}');do sudo docker rmi -f $i;done
 #Build new Nginx container
 sudo docker build  . -t ${repo_name}
 #Get Image ID
-sudo image_id=`docker images | grep ${repo_name} | awk '{print $3}'  | sort -u`
+image_id=`sudo docker images | grep ${repo_name} | awk '{print $3}'  | sort -u`
 sudo echo $image_id
 # Tag and Push to Amazon ECR
 sudo docker tag ${image_id} 522939339121.dkr.ecr.us-east-1.amazonaws.com/ecrrepo001/${repo_name}:latest
