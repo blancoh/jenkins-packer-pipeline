d84794077b25689a5c4c5f023f6f9017cdccf342
blancoh <blancoh@users.noreply.github.com>
2021-04-21 07:22:03 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "public_key_path" {
-   default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 variable "key_name" {
   description = "RSA key pair"
@@ -12,11 +12,12 @@ variable "aws_region" {
 # Ubuntu Bionic 18.04 LTS (x64)
 variable "aws_amis" {
   default = {
-    us-east-1 = "ami-026c8acd92718196b"
+  us-east-1 = "ami-072afab67fc409af2"
+    # us-east-1 = "ami-026c8acd92718196b"
   }
 }
 variable "vpc_cidr" {
-    default = "10.0.0.0/20"
+  default = "10.0.0.0/20"
   description = "the vpc cdir range"
 }
 variable "public_subnet_a" {
